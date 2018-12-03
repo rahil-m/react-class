c212d1142b2cfea037fc8c0f32531edb5d842317
Rahil M <mansurirahilm@gmail.com>
2018-12-03 00:05:07 -0800

    Search App - Section 7

---
+++ src/pics-section-7/components/App.js
@@ -0,0 +1,20 @@
+import * as React  from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+    state = {}
+
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    };
+}
+
+export default App;
---
+++ src/pics-section-7/index.js
@@ -0,0 +1,5 @@
+import * as React  from 'react';
+import * as ReactDOM  from 'react-dom';
+import App from './pics-section-7/components/App';
+
+ReactDOM.render(<App />, document.querySelector('#root'))
